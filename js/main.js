35127f43224b47049dc5cd79ee404ff30093e0ab
Nicolás Galdo <nicogaldo@gmail.com>
2016-05-23 14:14:51 -0300

    dashboard

--- js/main.js
+++ js/main.js
@@ -44,21 +44,27 @@ app.controller('selectItem', function($scope) {
   $scope.items = [
     {
       title: 'Inicio',
+      icon: 'home',
       href: '#/'
     }, { 
       title: 'Galería',
+      icon: 'picture-o',
       href: '#/galeria'
     }, { 
       title: 'Trabajo',
+      icon: 'briefcase',
       href: '#/trabajo'
     }, { 
       title: 'Foro',
+      icon: 'comments-o',
       href: '#/foro'
     }, { 
       title: 'Comunidad',
+      icon: 'users',
       href: '#/comunidad'
     }, { 
       title: 'Contacto',
+      icon: 'envelope-o',
       href: '#/contacto'
     }
   ];
